72f314c140ddf91474d3f423aa60473d113420e0
Said_Lutfullin <slutfullin@lincolnlabs.co>
2025-07-22 14:59:24 +0500

    q

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation,
 
 
 function App() {
-
+    // const apiUrl = 'http://localhost:5173/'
+    const apiUrl = 'https://testpdf-inky.vercel.app/'
   return (
-      <div style={{magrinTop: '1rem' }}>
+      <div styles={{magrinTop: '1rem' }}>
           <div className='control-section'>
               {/* Render the PDF Viewer */}
               <PdfViewerComponent
                   id="container"
                   documentPath="https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf"
-                  serviceUrl="https://services.syncfusion.com/react/production/api/pdfviewer"
+                  // serviceUrl="https://services.syncfusion.com/react/production/api/pdfviewer"
+                  resourceUrl={`${apiUrl || 'http://5.35.91.52:3000/'}/ej2-pdfviewer-lib`}
                   style={{ 'height': '640px' }}>
 
                   <Inject services={[ Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView,
